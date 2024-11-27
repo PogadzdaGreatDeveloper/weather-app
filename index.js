2f638e77b18e3a69d0a3d9fc5e0365fee07bc0ce
PogadzdaGreatDeveloper <pogadzpogado@gmail.com>
2024-11-28 02:20:30 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,506 @@
+var tableData = [
+    {
+        "_id": "66b1ceb0975027aeeb54af60",
+        "isActive": true,
+        "balance": "$1,203.55",
+        "picture": "http://placehold.it/32x32",
+        "age": 27,
+        "eyeColor": "brown",
+        "firstname": "Knapp",
+        "lastname": "Dunn",
+        "gender": "male",
+        "company": "ZENTIA",
+        "email": "knappdunn@zentia.com",
+        "phone": "+1 (943) 443-3894",
+        "address": "670 Lancaster Avenue, Brule, New Jersey, 2446",
+        "about": "Ipsum nisi officia laboris amet. Tempor pariatur commodo id Lorem pariatur dolore ea veniam occaecat velit. Ut sint est nisi tempor cupidatat cupidatat sit sunt laboris dolor ad elit. Do quis cillum aliquip mollit. Cillum aute incididunt cillum laboris.\r\n",
+        "registered": "2024-05-15T11:57:32 -08:00",
+        "latitude": -81.560282,
+        "longitude": -157.790539,
+        "tags": [
+            "aliquip",
+            "sunt",
+            "reprehenderit",
+            "reprehenderit",
+            "excepteur",
+            "ullamco",
+            "incididunt"
+        ],
+        "friends": [
+            {
+                "firstname": "Murphy",
+                "lastname": "Robles"
+            },
+            {
+                "firstname": "Bennett",
+                "lastname": "Christensen"
+            },
+            {
+                "firstname": "Johanna",
+                "lastname": "Roth"
+            },
+            {
+                "firstname": "Sophia",
+                "lastname": "Riley"
+            },
+            {
+                "firstname": "Reese",
+                "lastname": "Frost"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 1 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "66b1ceb06d01621c0b31d7c2",
+        "isActive": false,
+        "balance": "$1,762.31",
+        "picture": "http://placehold.it/32x32",
+        "age": 33,
+        "eyeColor": "blue",
+        "firstname": "Evangeline",
+        "lastname": "Mccarthy",
+        "gender": "female",
+        "company": "INQUALA",
+        "email": "evangelinemccarthy@inquala.com",
+        "phone": "+1 (962) 465-3640",
+        "address": "645 Grand Street, Hiko, South Carolina, 3170",
+        "about": "Commodo sint pariatur qui anim. Nostrud ullamco ex aliquip duis consectetur incididunt. Quis labore irure cupidatat qui fugiat ex proident occaecat labore quis velit minim qui. Officia duis eu cillum exercitation enim in elit laborum veniam eiusmod magna proident veniam. Cupidatat dolor ea minim culpa commodo ea mollit reprehenderit.\r\n",
+        "registered": "2023-12-19T01:58:40 -08:00",
+        "latitude": 62.718887,
+        "longitude": -166.447974,
+        "tags": [
+            "exercitation",
+            "esse",
+            "proident",
+            "ipsum",
+            "occaecat",
+            "est",
+            "voluptate"
+        ],
+        "friends": [
+            {
+                "firstname": "Foreman",
+                "lastname": "Hamilton"
+            },
+            {
+                "firstname": "Riddle",
+                "lastname": "Hays"
+            },
+            {
+                "firstname": "Vonda",
+                "lastname": "Buckner"
+            },
+            {
+                "firstname": "Polly",
+                "lastname": "Davis"
+            },
+            {
+                "firstname": "Guy",
+                "lastname": "Perez"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 4 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "66b1ceb0c8a7aee1d2f11b86",
+        "isActive": true,
+        "balance": "$3,434.69",
+        "picture": "http://placehold.it/32x32",
+        "age": 35,
+        "eyeColor": "brown",
+        "firstname": "Blanche",
+        "lastname": "Fitzpatrick",
+        "gender": "female",
+        "company": "INTERFIND",
+        "email": "blanchefitzpatrick@interfind.com",
+        "phone": "+1 (891) 499-2626",
+        "address": "260 Decatur Street, Grapeview, Montana, 116",
+        "about": "Laborum sint voluptate incididunt consequat ad. Proident in exercitation eu irure nisi aliqua nostrud ut. Cillum quis occaecat anim aliquip.\r\n",
+        "registered": "2015-06-30T12:30:27 -08:00",
+        "latitude": 56.355743,
+        "longitude": -13.758338,
+        "tags": [
+            "occaecat",
+            "est",
+            "irure",
+            "deserunt",
+            "ipsum",
+            "ullamco",
+            "fugiat"
+        ],
+        "friends": [
+            {
+                "firstname": "Graham",
+                "lastname": "Carter"
+            },
+            {
+                "firstname": "Nettie",
+                "lastname": "Sykes"
+            },
+            {
+                "firstname": "Huber",
+                "lastname": "Marquez"
+            },
+            {
+                "firstname": "Prince",
+                "lastname": "Griffith"
+            },
+            {
+                "firstname": "Cabrera",
+                "lastname": "Dorsey"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 9 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "66b1ceb0550c3aa429530fff",
+        "isActive": false,
+        "balance": "$3,046.16",
+        "picture": "http://placehold.it/32x32",
+        "age": 21,
+        "eyeColor": "blue",
+        "firstname": "Malone",
+        "lastname": "Ortega",
+        "gender": "male",
+        "company": "ORBIN",
+        "email": "maloneortega@orbin.com",
+        "phone": "+1 (928) 568-2960",
+        "address": "750 Congress Street, Coral, Tennessee, 7486",
+        "about": "Dolore consequat nulla officia adipisicing quis. Lorem Lorem id minim est. Aliqua Lorem in dolor excepteur laborum exercitation qui ex quis. Exercitation dolor eiusmod anim eiusmod deserunt reprehenderit nostrud amet aliquip deserunt cupidatat do proident. Eiusmod enim officia deserunt Lorem nulla consectetur Lorem do quis incididunt.\r\n",
+        "registered": "2022-10-12T05:21:20 -08:00",
+        "latitude": -49.497585,
+        "longitude": -107.59577,
+        "tags": [
+            "id",
+            "elit",
+            "nisi",
+            "pariatur",
+            "amet",
+            "aliquip",
+            "aute"
+        ],
+        "friends": [
+            {
+                "firstname": "Brigitte",
+                "lastname": "Mccall"
+            },
+            {
+                "firstname": "Cheri",
+                "lastname": "Wright"
+            },
+            {
+                "firstname": "Letitia",
+                "lastname": "Hughes"
+            },
+            {
+                "firstname": "Loraine",
+                "lastname": "Holder"
+            },
+            {
+                "firstname": "Calderon",
+                "lastname": "Bolton"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 1 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "66b1ceb04af72904f4df9cf6",
+        "isActive": true,
+        "balance": "$1,895.01",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "eyeColor": "green",
+        "firstname": "Suarez",
+        "lastname": "Mullen",
+        "gender": "male",
+        "company": "ETERNIS",
+        "email": "suarezmullen@eternis.com",
+        "phone": "+1 (950) 459-3086",
+        "address": "524 Livonia Avenue, Movico, Alabama, 7374",
+        "about": "Dolore commodo laboris nulla laborum sit laboris cillum dolor. Dolore voluptate aliqua officia ipsum eu incididunt ipsum sint veniam nostrud. Exercitation qui exercitation adipisicing incididunt elit nulla labore. Sunt dolore incididunt aliqua nulla. Enim nisi magna fugiat cupidatat laborum cillum nostrud enim do velit sint est amet dolor.\r\n",
+        "registered": "2017-05-15T07:13:10 -08:00",
+        "latitude": -81.020322,
+        "longitude": -163.077566,
+        "tags": [
+            "ipsum",
+            "eiusmod",
+            "ipsum",
+            "excepteur",
+            "pariatur",
+            "cillum",
+            "do"
+        ],
+        "friends": [
+            {
+                "firstname": "Chandra",
+                "lastname": "Anderson"
+            },
+            {
+                "firstname": "Katheryn",
+                "lastname": "Carrillo"
+            },
+            {
+                "firstname": "Battle",
+                "lastname": "Mcmillan"
+            },
+            {
+                "firstname": "Wolf",
+                "lastname": "Petty"
+            },
+            {
+                "firstname": "Branch",
+                "lastname": "Andrews"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 4 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "66b1ceb04de7ad387fbc523f",
+        "isActive": true,
+        "balance": "$2,052.09",
+        "picture": "http://placehold.it/32x32",
+        "age": 33,
+        "eyeColor": "green",
+        "firstname": "Lowery",
+        "lastname": "Cabrera",
+        "gender": "male",
+        "company": "ZIPAK",
+        "email": "lowerycabrera@zipak.com",
+        "phone": "+1 (856) 433-2838",
+        "address": "237 Imlay Street, Jardine, Kansas, 4469",
+        "about": "Reprehenderit fugiat labore enim deserunt eiusmod. Do pariatur esse laborum reprehenderit. Cupidatat nisi non id sunt in aliqua enim aute magna Lorem dolore voluptate pariatur. Anim occaecat anim ea ad.\r\n",
+        "registered": "2018-08-15T07:56:42 -08:00",
+        "latitude": -79.042461,
+        "longitude": -57.426387,
+        "tags": [
+            "laborum",
+            "nostrud",
+            "cupidatat",
+            "occaecat",
+            "dolore",
+            "voluptate",
+            "ea"
+        ],
+        "friends": [
+            {
+                "firstname": "Ingrid",
+                "lastname": "Madden"
+            },
+            {
+                "firstname": "Gladys",
+                "lastname": "Bray"
+            },
+            {
+                "firstname": "Oconnor",
+                "lastname": "Parker"
+            },
+            {
+                "firstname": "Hickman",
+                "lastname": "Merritt"
+            },
+            {
+                "firstname": "Maddox",
+                "lastname": "Nichols"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 1 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "66b1ceb0fd17015c09bee3bf",
+        "isActive": true,
+        "balance": "$2,269.93",
+        "picture": "http://placehold.it/32x32",
+        "age": 33,
+        "eyeColor": "brown",
+        "firstname": "Sullivan",
+        "lastname": "Lopez",
+        "gender": "male",
+        "company": "ANIXANG",
+        "email": "sullivanlopez@anixang.com",
+        "phone": "+1 (825) 458-2609",
+        "address": "352 Stockton Street, Caln, Idaho, 6766",
+        "about": "Voluptate laborum dolore fugiat qui adipisicing culpa. Veniam nulla incididunt et reprehenderit in esse. Enim ea consectetur officia qui consectetur anim eu reprehenderit. Sint commodo nostrud nulla consectetur nisi eiusmod nostrud duis laboris dolore est id officia laborum. Reprehenderit laboris esse magna sunt sunt officia elit ad adipisicing qui anim quis.\r\n",
+        "registered": "2016-06-27T10:38:50 -08:00",
+        "latitude": 75.516115,
+        "longitude": -83.392644,
+        "tags": [
+            "laboris",
+            "cillum",
+            "laborum",
+            "nisi",
+            "Lorem",
+            "elit",
+            "nulla"
+        ],
+        "friends": [
+            {
+                "firstname": "Cruz",
+                "lastname": "Randolph"
+            },
+            {
+                "firstname": "Flossie",
+                "lastname": "Johns"
+            },
+            {
+                "firstname": "Kerri",
+                "lastname": "Curtis"
+            },
+            {
+                "firstname": "Weber",
+                "lastname": "Tate"
+            },
+            {
+                "firstname": "Beatriz",
+                "lastname": "Gregory"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 3 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "66b1ceb01f3da4e3834e27ba",
+        "isActive": true,
+        "balance": "$3,563.93",
+        "picture": "http://placehold.it/32x32",
+        "age": 23,
+        "eyeColor": "brown",
+        "firstname": "Sybil",
+        "lastname": "Kerr",
+        "gender": "female",
+        "company": "MANTRIX",
+        "email": "sybilkerr@mantrix.com",
+        "phone": "+1 (882) 537-2559",
+        "address": "216 Hutchinson Court, Fostoria, Ohio, 4783",
+        "about": "Sit cupidatat esse in laborum eu adipisicing. Laborum occaecat ut enim proident ut reprehenderit exercitation excepteur. Excepteur sit do laboris occaecat exercitation magna labore. Do culpa sint magna magna sunt non dolore sint ipsum dolore officia elit tempor. Adipisicing amet ut do aute nostrud. Pariatur sint consectetur ut laboris fugiat exercitation amet pariatur minim. Qui magna velit consectetur velit irure.\r\n",
+        "registered": "2022-08-31T01:51:08 -08:00",
+        "latitude": -84.875043,
+        "longitude": -124.506086,
+        "tags": [
+            "ea",
+            "voluptate",
+            "tempor",
+            "elit",
+            "aliquip",
+            "tempor",
+            "sint"
+        ],
+        "friends": [
+            {
+                "firstname": "Susan",
+                "lastname": "Vang"
+            },
+            {
+                "firstname": "Pauline",
+                "lastname": "Trujillo"
+            },
+            {
+                "firstname": "Leah",
+                "lastname": "Pearson"
+            },
+            {
+                "firstname": "Earnestine",
+                "lastname": "Carey"
+            },
+            {
+                "firstname": "Janie",
+                "lastname": "Richardson"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 7 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "66b1ceb00577eee7a03e56ad",
+        "isActive": false,
+        "balance": "$3,818.84",
+        "picture": "http://placehold.it/32x32",
+        "age": 31,
+        "eyeColor": "green",
+        "firstname": "Robin",
+        "lastname": "Melendez",
+        "gender": "female",
+        "company": "ISOSURE",
+        "email": "robinmelendez@isosure.com",
+        "phone": "+1 (939) 420-2979",
+        "address": "385 Emmons Avenue, Kenmar, New Hampshire, 2193",
+        "about": "Nostrud irure occaecat commodo labore cupidatat enim commodo. Mollit commodo voluptate non dolore sunt amet commodo. Anim sunt voluptate do elit adipisicing id cillum officia ullamco incididunt. Lorem fugiat dolore ex dolor consectetur aute sunt duis eu ea et. Dolore sunt culpa voluptate consectetur et incididunt sunt Lorem consequat deserunt reprehenderit laboris do. Veniam commodo dolor reprehenderit sit consequat deserunt aute eu fugiat sunt in minim ipsum anim. Nostrud quis elit dolor incididunt cillum velit adipisicing.\r\n",
+        "registered": "2020-06-19T01:42:04 -08:00",
+        "latitude": 62.764448,
+        "longitude": 93.97437,
+        "tags": [
+            "incididunt",
+            "reprehenderit",
+            "ad",
+            "esse",
+            "Lorem",
+            "occaecat",
+            "eiusmod"
+        ],
+        "friends": [
+            {
+                "firstname": "Briggs",
+                "lastname": "Castaneda"
+            },
+            {
+                "firstname": "Gross",
+                "lastname": "Sosa"
+            },
+            {
+                "firstname": "Leanne",
+                "lastname": "Bentley"
+            },
+            {
+                "firstname": "Stanley",
+                "lastname": "Freeman"
+            },
+            {
+                "firstname": "Aline",
+                "lastname": "Rush"
+            }
+        ],
+        "greeting": "Hello, undefined! You have 10 unread messages.",
+        "favoriteFruit": "banana"
+    }
+];
+
+
+for(var i in tableData){
+    var friendsList = "";
+    for(var x in tableData[i].friends){
+        friendsList += tableData[i].friends[x].firstname + " " + tableData[i].friends[x].lastname + "<br>";
+    }
+
+    var row = `<tr>
+        <td>${tableData[i]._id}</td>
+        <td>${tableData[i].isActive}</td>
+        <td>${tableData[i].balance}</td>
+        <td>${tableData[i].picture}</td>
+        <td>${tableData[i].age}</td>
+        <td>${tableData[i].eyeColor}</td>
+        <td>${tableData[i].firstname}</td>
+        <td>${tableData[i].lastname}</td>
+        <td>${tableData[i].gender}</td>
+        <td>${tableData[i].company}</td>
+        <td>${tableData[i].email}</td>
+        <td>${tableData[i].phone}</td>
+        <td>${tableData[i].address}</td>
+        <td>${tableData[i].about}</td>
+        <td>${tableData[i].registered}</td>
+        <td>${tableData[i].latitude}</td>
+        <td>${tableData[i].longitude}</td> 
+        <td>${tableData[i].tags}</td>
+        <td>${friendsList}</td>
+        <td>${tableData[i].greeting}</td>
+        <td>${tableData[i].favoriteFruit}</td>
+    </tr>`;
+
+    var table = $('#table-body');
+    table.append(row);
+}
+
